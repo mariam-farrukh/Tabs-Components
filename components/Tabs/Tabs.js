ecd0568152e29b598734fcf4559912dfeaa183fe
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-06-20 14:30:20 -0500

    completed constructor of class TabLink

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -3,18 +3,18 @@ class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
     // this.element;
-    
+    this.element = element
     // Get the custom data attribute on the Link
     // this.data;
-    
+    this.data = this.element.dataset.tab;
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
-    
+    this.itemElement = documents.querySelector(`.tabs-item[data-tab='${this.data}']`);
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
-    
+    this.tabItem = new TabItem(this.itemElement);
     // Add a click event listener on this instance, calling the select method on click
-
+    this.element.addEventListener('click', () => this.select());
   };
 
   select() {
@@ -53,10 +53,15 @@ class TabItem {
 
 - Select all classes named ".tabs-link" and assign that value to the links variable
 
-- With your selection in place, now chain a .forEach() method onto the links variable to iterate over the DOM NodeList
+- With your selection in place, now chain a .forEach() method onto the links 
+  variable to iterate over the DOM NodeList
 
-- In your .forEach() method's callback function, return a new instance of TabLink and pass in each link as a parameter
+- In your .forEach() method's callback function, return a new instance of TabLink 
+  and pass in each link as a parameter
 
 */
 
-links = document.querySelectorAll();
+let links = document.querySelectorAll(".tab-links");
+links.forEach(function(link){
+  return new TabLink (link);
+});
